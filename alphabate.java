b3abd7e7712b9460ac2968b9cb7252a0c9d0b511
vaishnavi thakare <159428762+Vaish-2001@users.noreply.github.com>
2024-02-23 12:06:44 +0530

    Add files via upload

---
+++ alphabate.java
@@ -0,0 +1,10 @@
+class Alphabate
+{
+ public static void main(String[]args)
+ {
+  char c='d';
+ 
+  String result1=(c>=65&&c<=90||c>=97&&c<=122)?("this is alphabate"):("this is not alphabate");
+  System.out.println(result1);
+ }
+}
